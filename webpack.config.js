941e67ad2efb34e24bce75f45607f957181bc2d7
WinterCore <hogobbl@gmail.com>
2020-10-25 22:08:12 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    port: 8080,
+    contentBase: 'dist',
+    open: true,
+  },
+  output: {
+    filename: '[hash]_bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+  ],
+};
